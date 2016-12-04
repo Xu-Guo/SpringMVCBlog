3316d671fed0cc880452ec61a616189a545093fd
Xu-Guo <xuguo2002@gmail.com>
2016-12-04 12:50:37 -0800

    Finished addBlog.jsp, add addBlog function to BlogController and tested the function.

--- src/main/java/com/xuguo/controller/BlogController.java
+++ src/main/java/com/xuguo/controller/BlogController.java
@@ -1,10 +1,13 @@
 package com.xuguo.controller;
 
 import com.xuguo.model.BlogEntity;
+import com.xuguo.model.UserEntity;
 import com.xuguo.repository.BlogRepository;
+import com.xuguo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,6 +21,9 @@ public class BlogController {
     @Autowired
     BlogRepository blogRepository;
 
+    @Autowired
+    UserRepository userRepository;
+
     //show all articles
     @RequestMapping(value = "admin/blogs", method = RequestMethod.GET)
     public String showBlogs(ModelMap modelMap) {
@@ -25,4 +31,28 @@ public class BlogController {
         modelMap.addAttribute("blogList", blogList);
         return "admin/blogs";
     }
+
+    //add blog
+    @RequestMapping(value = "admin/blogs/add", method = RequestMethod.GET)
+    public String addBlog(ModelMap modelMap) {
+        List<UserEntity> userList = userRepository.findAll();
+        //inject user list
+        modelMap.addAttribute("userList", userList);
+        return "admin/addBlog";
+    }
+
+    //add blog, use http post method, jump to blogs.jsp
+
+    @RequestMapping(value = "admin/blogs/addP", method = RequestMethod.POST)
+    public String addBlogPost(@ModelAttribute("blog") BlogEntity blogEntity) {
+        //print blog title
+        System.out.println(blogEntity.getTitle());
+        //print blog author
+        System.out.println(blogEntity.getUserByUserId().getNickname());
+        //insert into database
+        blogRepository.saveAndFlush(blogEntity);
+        //redirect to blogs.jsp
+        return "redirect:/admin/blogs";
+    }
+
 }
