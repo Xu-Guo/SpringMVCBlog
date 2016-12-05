ca5cbf0ec750b4f326e059ad0c5224dcf2ce5bd5
xuxu <xuguo2002@gmail.com>
2016-12-04 22:58:59 -0800

    add in Interceptor, the interceptor works, but the return jsp has problem.

--- src/main/java/com/xuguo/interceptor/AuthInterceptor.java
+++ src/main/java/com/xuguo/interceptor/AuthInterceptor.java
@@ -6,6 +6,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
  * Created by xuxu on 12/4/16.
  */
